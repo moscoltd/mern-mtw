65c661458b0cfc0343a137592c64c13ce7258059
moscoltd <moscooinnovatorsltd@gmail.com>
2024-01-10 17:41:51 +0300

    email verification MEARN

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+//console log
+console.log("DB URI:", process.env.DB);
+console.log("jwt:", process.env.JWTPRIVATEKEY);
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
